9a38014e5625502f104947dc0c08df20f872f7dd
Annie Phan <anniephan.atp@gmail.com>
2019-05-31 02:37:28 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :verify_user_is_authenticated
+  helper_method :current_user, :require_login, :admin_only
+
+  def current_user
+    if session[:user_id].present?
+      user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_login
+    unless current_user
+      redirect_to root_url
+    end
+  end
+
+  def admin_only
+    unless current_user.admin
+      flash[:notice] = "You must be an admin to perform that function!"
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def verify_user_is_authenticated
+    redirect_to '/' unless user_is_authenticated
+  end
+
+  def user_is_authenticated
+    !!current_user
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root "static_pages#home"
+  # resources :users, only: [:new, :create, :show]
+  #
+  # get '/signin', to: 'sessions#new'
+  # post '/signin', to: 'sessions#create'
+  # delete '/logout', to: 'session#destroy'
+  #
+  # # get '/signin', to: 'users#signin'
+  # # get '/login', to: 'user#login'
+  # # get '/signout', to: 'users#signout'
+  # resources :attractions
+  #
+  # post '/rides', to: 'rides#create'
+
+  root 'static#homepage'
+  get '/signin', to: 'sessions#new'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/sessions/delete', to: 'sessions#destroy'
+
+  resources :rides
+  resources :attractions
+  resources :users
 end
