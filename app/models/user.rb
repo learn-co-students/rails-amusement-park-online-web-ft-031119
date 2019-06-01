06685207f579231684efc87202affe1ea0a64f24
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-01 10:40:33 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    if self.nausea && self.happiness
+      if self.nausea > self.happiness
+        "sad"
+      elsif self.nausea == self.happiness
+        "bored"
+      else
+        "happy"
+      end
+    end
+  end
 end
