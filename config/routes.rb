9357baa9c0667df3f30320522d551a7c938f4de7
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-01 16:19:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   resources :users, only: [:new, :create, :show]
   resources :attractions
-  get '/signin', to: 'session#new'
-  post '/signin', to: 'session#create'
-  delete '/session', to: 'session#destroy'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   post '/rides', to: 'rides#create'
 end
